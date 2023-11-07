65dc10c88e9c3ee73f9611b8d3e996a7b7084851
Baptiste <baptiste.noll@adeliom.com>
2023-11-07 11:40:28 +0100

    feat(navBar): add drawer logo and begin topNavBar

---
+++ src/stories/topNavBar.stories.ts
@@ -0,0 +1,18 @@
+import { html } from 'lit';
+import type { Meta, StoryObj } from '@storybook/web-components';
+
+export default {
+  title: 'Block/Header/TopNavBar',
+  parameters: {
+    layout: ''
+  }
+} satisfies Meta;
+
+export const NavBar1: StoryObj = {
+    name: 'TopNavBar 1',
+    render: () => html`
+        <div>
+            erferf
+        </div>
+    `
+}
